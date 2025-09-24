c082e0fc6514a4a3d204e03dba0b04aa2ba6b97d
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-09-24 16:47:46 -0500

    feat: update SQL Server setup for improved compatibility and permissions
    Switch to mssql-tools18 in attach_db.sh for better compatibility, add verbose output, and enable connection encryption. Update docker-compose.yml to set Developer edition, enable SQL Agent, specify mssql user, and set explicit read-write volume permissions for improved security and reliability.

--- attach_db.sh
+++ attach_db.sh
@@ -2,9 +2,12 @@
 # Wait for SQL Server to be ready (adjust the time if necessary)
 sleep 15s
 
+echo ">>> Attaching the database..."
+
 # Try to attach the database
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" \
-    -Q "CREATE DATABASE real_db ON (FILENAME = '/var/opt/mssql/data/real_db.mdf'), (FILENAME = '/var/opt/mssql/data/real_db.ldf') FOR ATTACH;"
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -C \
+    -Q "CREATE DATABASE real_db ON (FILENAME = '/var/opt/mssql/data/real_db.mdf'), \
+        (FILENAME = '/var/opt/mssql/data/real_db.ldf') FOR ATTACH;"
 
 # Keep the script alive (optionally you could use a loop or just exit)
 /bin/bash
