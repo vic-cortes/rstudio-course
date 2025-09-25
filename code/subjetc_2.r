df350e01f06151d774e829714c922ab9e5c56959
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-09-24 22:57:53 -0500

    feat(data): add readr package and improve CSV import examples
    Added 'readr' to package list and expanded CSV import examples with various encoding options and usage of read_csv for better data handling and clarity.

--- code/subjetc_2.r
+++ code/subjetc_2.r
@@ -14,7 +14,8 @@ pacman::p_load(
   "readxl",
   "haven",
   "foreign",
-  "lubridate"
+  "lubridate",
+  "readr"
 )
 
 # Download data from https://inegi.org.mx/programas/envipe/2025/#microdatos
@@ -23,10 +24,35 @@ ENVIPE_FILE <- "db/TSDem.csv"
 df <- read.csv(ENVIPE_FILE)
 names(df)
 
-# Datos de series de tiempo
-DAILY_SIMPLE_FILE <- "db/afluenciastc_simple_08_2025.csv"
+# Diario Oficial de la Federacion
 
-# Diraro Ofcial de la Federacion
 TC_DOF <- "db/TC_DOF.csv"
 df <- read.csv(TC_DOF, skip = 29)
 colnames(df) <- c("Fecha", "TC_DOF")
+
+# Convert column to date and extract year
+df <- df |>
+  mutate(
+    Fecha = as.Date(Fecha, format = "%d/%m/%Y"),
+    year = format(Fecha, "%Y")
+  )
+
+# Datos de panel
+
+# Datos de series de tiempo
+DAILY_SIMPLE_FILE <- "db/afluenciastc_simple_08_2025.csv"
+
+df <- read.csv(DAILY_SIMPLE_FILE, encoding = "latin1", stringsAsFactors = FALSE)
+df <- read.csv(
+  DAILY_SIMPLE_FILE,
+  fileEncoding = "UTF-8",
+  stringsAsFactors = FALSE
+)
+df <- read_csv(DAILY_SIMPLE_FILE, locale = locale(encoding = "Latin1"))
+
+# Other examples
+
+EXAMPLE_FILE <- "db/IDEFC_NM_ago25.csv"
+
+df <- read.csv(EXAMPLE_FILE, encoding = "latin1")
+df |> head()
