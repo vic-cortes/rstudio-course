ffbfb9f2e40fccb5af750c00320845b519da24fa
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-09-24 22:03:43 -0500

    chore: update .gitignore for CSV files and add data loading in R script
    Added *.csv to .gitignore to exclude CSV data files from version control. Updated subjetc_2.r to load multiple CSV datasets for analysis, improving data handling and reproducibility.

--- code/subjetc_2.r
+++ code/subjetc_2.r
@@ -16,3 +16,17 @@ pacman::p_load(
   "foreign",
   "lubridate"
 )
+
+# Download data from https://inegi.org.mx/programas/envipe/2025/#microdatos
+ENVIPE_FILE <- "db/TSDem.csv"
+
+df <- read.csv(ENVIPE_FILE)
+names(df)
+
+# Datos de series de tiempo
+DAILY_SIMPLE_FILE <- "db/afluenciastc_simple_08_2025.csv"
+
+# Diraro Ofcial de la Federacion
+TC_DOF <- "db/TC_DOF.csv"
+df <- read.csv(TC_DOF, skip = 29)
+colnames(df) <- c("Fecha", "TC_DOF")
