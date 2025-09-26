15556f06f66010b71279d59c65b1f3be54cd3d6b
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-09-25 21:56:42 -0500

    feat(subject_3): add filters for homicide types and improve CSV reading
    Refactored CSV import to disable name checking and added filters to view records for "Homicidio doloso" and "Homicidio culposo" subtypes, enhancing data exploration.

--- code/subject_3.R
+++ code/subject_3.R
@@ -20,4 +20,21 @@ pacman::p_load(
 
 INCIDENCIA_DELICTIVA_FILE <- "db/IDEFC_NM_ago25.csv"
 
-df <- read.csv(INCIDENCIA_DELICTIVA_FILE, sep = ",", encoding = "latin1")
+df <- read.csv(
+  INCIDENCIA_DELICTIVA_FILE,
+  check.names = FALSE,
+  encoding = "latin1"
+)
+
+# Filters
+homicidio_doloso <- "Homicidio doloso"
+homicidio_culposo <- "Homicidio culposo"
+
+df %>% filter(`Subtipo de delito` == homicidio_doloso) %>% view()
+
+# Or filter with multiple conditions
+df %>%
+  filter(
+    `Subtipo de delito` %in% c(homicidio_doloso, homicidio_culposo)
+  ) %>%
+  view()
