414d5de41996825e89041ae844f4756be7602a6a
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-10-02 22:05:12 -0500

    feat(session_8): add font loading and plot for TopSpeed vs Range
    Added font import and loading for all devices. Introduced a ggplot visualization of TopSpeed_KmH vs Range_Km with custom colors, smoothing, and axis limits to enhance data analysis and presentation.

--- code/course/session_8.R
+++ code/course/session_8.R
@@ -38,7 +38,24 @@ pacman::p_load(
   "extrafont"
 )
 
+loadfonts(device = "all")
+font_import()
+
 
 FILE_NAME <- "db/ElectricCarData_Clean.csv"
 
+color <- list(BLUE = "#1C69A8", RED = "#E7180B")
+
 df <- read_csv(FILE_NAME)
+
+df |>
+  ggplot(aes(x = TopSpeed_KmH, y = Range_Km)) +
+  geom_smooth(
+    method = "lm",
+    formula = y ~ splines::bs(x, 3),
+    se = FALSE,
+    color = color$RED
+  ) +
+  geom_point(shape = 17, size = 3, color = color$BLUE) +
+  scale_x_continuous(breaks = seq(100, 420, 20), limits = c(100, 420)) +
+  scale_y_continuous(breaks = seq(50, 1000, 50), limits = c(50, 1000))
