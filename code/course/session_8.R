74728c69392ace3348daff19af335289a0e7eb8b
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-10-02 22:28:46 -0500

    feat(plot): highlight cars with range >550km and annotate on plot
    Added logic to filter and label electric cars exceeding 550km range. Updated plot to display annotations and arrows for these cars, improving data visualization and insight.

--- code/course/session_8.R
+++ code/course/session_8.R
@@ -44,17 +44,37 @@ font_import()
 
 FILE_NAME <- "db/ElectricCarData_Clean.csv"
 
-color <- list(BLUE = "#1C69A8", RED = "#E7180B")
+color <- list(BLUE = "#1C69A8", RED = "#E7180B", BLACK = "black")
 
 df <- read_csv(FILE_NAME)
+
+punto_max <- df |> filter(Range_Km == max(Range_Km))
+punto_max <- df |> filter(Range_Km > 550)
 
 df |>
   ggplot(aes(x = TopSpeed_KmH, y = Range_Km)) +
   geom_smooth(
     method = "lm",
-    formula = y ~ splines::bs(x, 3),
+    # formula = y ~ splines::bs(x, 3),
     se = FALSE,
     color = color$RED
+  ) +
+  geom_text(
+    data = punto_max,
+    aes(label = paste(Brand, "-", Model)),
+    vjust = -1,
+    hjust = -0.1,
+    color = color$BLACK
+  ) +
+  annotate(
+    "segment",
+    x = punto_max$TopSpeed_KmH + 10,
+    y = punto_max$Range_Km + 10,
+    xend = punto_max$TopSpeed_KmH + 1,
+    yend = punto_max$Range_Km + 1,
+    arrow = arrow(length = unit(0.2, "cm")),
+    color = color$BLACK,
+    size = 1.3
   ) +
   geom_point(shape = 17, size = 3, color = color$BLUE) +
   scale_x_continuous(breaks = seq(100, 420, 20), limits = c(100, 420)) +
