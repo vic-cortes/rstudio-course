7fca00ac80faa2e8cc6c61ff3bfd5dc525f083b1
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-10-05 10:41:50 -0500

    refactor(session_8): extract plotting logic into reusable function
    Moved ggplot code into a create_graph() function to improve code reuse and readability. Now supports plotting for multiple filtered datasets, making annotation and visualization more flexible.

--- code/course/session_8.R
+++ code/course/session_8.R
@@ -56,33 +56,43 @@ df <- read_csv(FILE_NAME)
 punto_max <- df |> filter(Range_Km == max(Range_Km))
 punto_min <- df |> filter(Range_Km == min(Range_Km))
 punto_max <- df |> filter(Range_Km > 550)
+all_points <- df |> filter(Range_Km > 550 | Range_Km == min(Range_Km))
 
-df |>
-  ggplot(aes(x = TopSpeed_KmH, y = Range_Km)) +
-  geom_smooth(
-    method = "lm",
-    # formula = y ~ splines::bs(x, 3),
-    se = FALSE,
-    color = colors$RED
-  ) +
-  geom_text(
-    data = punto_max,
-    aes(label = paste(Brand, "-", Model)),
-    vjust = -1,
-    hjust = -0.1,
-    color = colors$BLACK,
-    fontface = "bold"
-  ) +
-  annotate(
-    "segment",
-    x = punto_max$TopSpeed_KmH + 10,
-    y = punto_max$Range_Km + 10,
-    xend = punto_max$TopSpeed_KmH + 2,
-    yend = punto_max$Range_Km + 2,
-    arrow = arrow(length = unit(0.2, "cm")),
-    color = colors$BLACK,
-    size = 1.3
-  ) +
-  geom_point(shape = 17, size = 3, color = colors$BLUE) +
-  scale_x_continuous(breaks = seq(100, 420, 20), limits = c(100, 420)) +
-  scale_y_continuous(breaks = seq(50, 1000, 50), limits = c(50, 1000))
+create_graph <- function(selected_data) {
+  # Creates a scatter plot with a linear model fit and annotations for
+  # the maximum range car
+
+  df |>
+    ggplot(aes(x = TopSpeed_KmH, y = Range_Km)) +
+    geom_smooth(
+      method = "lm",
+      # formula = y ~ splines::bs(x, 3),
+      se = FALSE,
+      color = colors$RED
+    ) +
+    geom_text(
+      data = selected_data,
+      aes(label = paste(Brand, "-", Model)),
+      vjust = -1,
+      hjust = -0.1,
+      color = colors$BLACK,
+      fontface = "bold"
+    ) +
+    annotate(
+      "segment",
+      x = selected_data$TopSpeed_KmH + 10,
+      y = selected_data$Range_Km + 10,
+      xend = selected_data$TopSpeed_KmH + 2,
+      yend = selected_data$Range_Km + 2,
+      arrow = arrow(length = unit(0.2, "cm")),
+      color = colors$BLACK,
+      size = 1.3
+    ) +
+    geom_point(shape = 17, size = 3, color = colors$BLUE) +
+    scale_x_continuous(breaks = seq(100, 420, 20), limits = c(100, 420)) +
+    scale_y_continuous(breaks = seq(50, 1000, 50), limits = c(50, 1000))
+}
+
+create_graph(punto_max)
+create_graph(punto_min)
+create_graph(all_points)
