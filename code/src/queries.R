fd8062c18ec8b7fd69d922545881bda8f96f687e
Victor Cortes Figueroa <vmcortesf@gmail.com>
2025-10-06 23:13:34 -0500

    feat(queries): add DbDataFetcher R6 class for flexible date queries
    Refactored date-based queries and introduced DbDataFetcher R6 class to generalize data retrieval by date range. Improves code reusability and clarity for fetching checksum and error data.

--- code/src/queries.R
+++ code/src/queries.R
@@ -8,7 +8,11 @@ response <- dbSendQuery(conn, "SELECT * FROM data WHERE LEN(error) > 0")
 # Create a query by date
 response <- dbSendQuery(
   conn,
-  "SELECT * FROM data WHERE Fecha >= '2025-08-01' AND Fecha <= '2025-08-31'"
+  "SELECT * FROM checksum WHERE fechasvr >= '2023-08-01 00:00:00' AND fechasvr <= '2023-08-31 23:59:59'"
+)
+response <- dbSendQuery(
+  conn,
+  "SELECT * FROM data WHERE fecha >= '2023-08-01 00:00:00' AND fecha <= '2023-08-31 23:59:59'"
 )
 
 get_checksum_data_by_date <- function(start_date, end_date) {
@@ -19,10 +23,60 @@ get_checksum_data_by_date <- function(start_date, end_date) {
     end_date
   )
   response <- dbSendQuery(conn, query)
-  df <- dbFetch(response) |> as_tibble()
+  df <- dbFetch(response) |> dplyr::as_tibble()
   dbClearResult(response)
   return(df)
 }
+
+
+DbDataFetcher <- R6Class(
+  "DbDataFetcher",
+  public = list(
+    initialize = function() {},
+
+    get_data_by_date = function(
+      table,
+      date_field,
+      start_date,
+      end_date
+    ) {
+      # Returns a tibble with data between start_date and end_date
+
+      query <- glue::glue(
+        "SELECT * FROM {table} WHERE {date_field} >= '{start_date}' AND {date_field} <= '{end_date}'"
+      )
+
+      response <- dbSendQuery(conn, query)
+      df <- dbFetch(response) |> dplyr::as_tibble()
+      dbClearResult(response)
+      return(df)
+    },
+
+    get_checksum_data_by_date = function(start_date, end_date) {
+      TABLE <- "checksum"
+      DATE_FIELD <- "fechasvr"
+
+      return(self$get_data_by_date(
+        TABLE,
+        DATE_FIELD,
+        start_date,
+        end_date
+      ))
+    },
+
+    get_error_data_by_date = function(start_date, end_date) {
+      TABLE <- "data"
+      DATE_FIELD <- "fecha"
+
+      return(self$get_data_by_date(
+        TABLE,
+        DATE_FIELD,
+        start_date,
+        end_date
+      ))
+    }
+  )
+)
 
 
 # Traer, por ejemplo, 1000 filas
